a44a92c827087a43ce6598d20673dfa9be42ee9e
David Millman <dave@cs.unc.edu>
2018-01-30 10:41:22 -0700

    Fix spacing

--- labs/lab2/main.cpp
+++ labs/lab2/main.cpp
@@ -29,10 +29,10 @@ GLFWwindow* initWindow() {
     }
 
 #ifdef __APPLE__
-    glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    glfwWindowHint (GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint (GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 #endif
 
     window = glfwCreateWindow(640, 480, "Lab 2", NULL, NULL);
