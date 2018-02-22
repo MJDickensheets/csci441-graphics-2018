0513e502c5029bf63b8dea196558f07886c717e4
Michael Dickensheets <Dick@Michaels-MacBook-Pro-2.local>
2018-02-22 14:49:43 -0700

    Lab 4 through part 3

--- labs/lab4/main.cpp
+++ labs/lab4/main.cpp
@@ -11,15 +11,69 @@
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
+int view_y = 0;
 
 void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
 }
 
-void processInput(GLFWwindow *window, Shader &shader) {
+char processInput(GLFWwindow *window, Shader &shader) {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
         glfwSetWindowShouldClose(window, true);
     }
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+	return 'w';
+    }
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+	return 's';
+    }
+    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
+	return '1';
+    }   
+    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+	return '2';
+    }   
+    if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
+	return '3';
+    }   
+    if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
+	return '4';
+    }   
+    if (glfwGetKey(window, GLFW_KEY_COMMA) == GLFW_PRESS) {
+	return ',';
+    }
+    if (glfwGetKey(window, GLFW_KEY_PERIOD) == GLFW_PRESS) {
+	return '.';
+    }
+    if (glfwGetKey(window, GLFW_KEY_MINUS) == GLFW_PRESS) {
+	return '-';
+    }
+    if (glfwGetKey(window, GLFW_KEY_EQUAL) == GLFW_PRESS) {
+	return '=';
+    }
+    if (glfwGetKey(window, GLFW_KEY_U) == GLFW_PRESS) {
+	return 'u';
+    }
+    if (glfwGetKey(window, GLFW_KEY_I) == GLFW_PRESS) {
+	return 'i';
+    }
+    if (glfwGetKey(window, GLFW_KEY_O) == GLFW_PRESS) {
+	return 'o';
+    }
+    if (glfwGetKey(window, GLFW_KEY_P) == GLFW_PRESS) {
+	return 'p';
+    }
+    if (glfwGetKey(window, GLFW_KEY_LEFT_BRACKET) == GLFW_PRESS) {
+	return '[';
+    }
+    if (glfwGetKey(window, GLFW_KEY_RIGHT_BRACKET) == GLFW_PRESS) {
+	return ']';
+    }
+    if (glfwGetKey(window, GLFW_KEY_BACKSLASH) == GLFW_PRESS) {
+	    return '\\';
+    }
+
+    return '~';
 }
 
 void errorCallback(int error, const char* description) {
@@ -131,11 +185,150 @@ int main(void) {
 
     // and use z-buffering
     glEnable(GL_DEPTH_TEST);
+    
+    float nx = 640;
+    float ny = 480;
+
+    float l = -0.9;
+    float r = 0.9;
+    float b = -0.9;
+    float t = 0.9;
+    float n = 1;
+    float f = -40;
+
+    float thetax = 0;    
+    float thetay = 0;
+    float thetaz = 0;
+
+    int projvalue = false;
+    //set view
+    float view[] = {
+	    1, 0, 0, 0,
+	    0, 1, 0, 0, 
+	    0, 0, 1, 0,
+	    0, 0, -1, 1};
+
+    //set model
+    float model[] = {
+	    1, 0, 0, 0,
+	    0, 1, 0, 0,
+	    0, 0, 1, 0,
+	    0, 0, 0, 1};
+
+    //rotation matrix
+    float rotatex[] = {
+	    1, 0, 0, 0,
+	    0, 1, 0, 0,
+	    0, 0, 1, 0,
+	    0, 0, 0, 1};   
+    float rotatey[] = {
+	    1, 0, 0, 0,
+	    0, 1, 0, 0,
+	    0, 0, 1, 0,
+	    0, 0, 0, 1};
+    float rotatez[] = {
+	    1, 0, 0, 0,
+	    0, 1, 0, 0,
+	    0, 0, 1, 0,
+	    0, 0, 0, 1};
+    //ortho matrix
+    float ortho[] = {
+	    2/(r-l), 0, 0, 0,
+	    0, 2/(t-b), 0, 0,
+	    0, 0, -2/(-n+f), 0,
+	    -(r+l)/(r-l), -(t+b)/(t-b), -(n+f)/(-n+f), 1};
+    //proj matrix
+    float project[] = {
+	   2*std::abs(n)/(r-l), 0, 0, 0,
+	   0, 2*std::abs(n)/(t-b), 0, 0, 
+	   (r+l)/(r-l), (t+b)/(t-b), (std::abs(n)+std::abs(f))/(std::abs(n)-std::abs(f)), -1,
+	   0, 0, 2*std::abs(f)*std::abs(n)/(std::abs(n)-std::abs(f)), 0};
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
         // process input
-        processInput(window, shader);
+        char input = processInput(window, shader);
+	if(input == 'w'){
+		view[13] += 0.05;
+	}
+	else if (input == 's'){
+		view[13] -= 0.05;
+	}
+	else if (input == '1'){
+		view[13] += 0.02;
+	}
+	else if (input == '2'){
+		view[13] -= 0.02;
+	}
+	else if (input == '3'){
+		model[12] -= 0.02;
+	}
+	else if (input == '4'){
+		model[12] += 0.02;
+	}
+	else if (input == ','){
+		view[14] -= 0.02;
+	}
+	else if (input == '.'){
+		view[14] += 0.02;
+	}
+	else if (input == '-'){
+		view[15] = view[15] * (1.25);
+	}
+	else if (input == '='){
+		view[15] = view[15] * (0.8);
+	}
+	else if (input == 'u'){
+		thetax -= 0.02;
+		rotatex[5] = (float)cos(thetax);
+		rotatex[9] = (float)-sin(thetax);
+		rotatex[6] = (float)sin(thetax);
+		rotatex[10] = (float)cos(thetax);
+	}
+	else if (input == 'i'){
+		thetax += 0.02;
+		rotatex[5] = (float)cos(thetax);
+		rotatex[9] = (float)-sin(thetax);
+		rotatex[6] = (float)sin(thetax);
+		rotatex[10] = (float)cos(thetax);
+	}
+	else if (input == 'o'){
+		thetay -= 0.02;
+		rotatey[0] = (float)cos(thetay);
+		rotatey[2] = (float)-sin(thetay);
+		rotatey[8] = (float)sin(thetay);
+		rotatey[10] = (float)cos(thetay);
+
+	}
+	else if (input == 'p'){
+		thetay += 0.02;
+		rotatey[0] = (float)cos(thetay);
+		rotatey[2] = (float)-sin(thetay);
+		rotatey[8] = (float)sin(thetay);
+		rotatey[10] = (float)cos(thetay);
+	}
+	else if (input == '['){
+		thetaz -= 0.02;
+		rotatez[0] = (float)cos(thetaz);
+		rotatez[4] = (float)-sin(thetaz);
+		rotatez[1] = (float)sin(thetaz);
+		rotatez[5] = (float)cos(thetaz);
+	}
+	else if (input == ']'){
+		thetaz += 0.02;
+		rotatez[0] = (float)cos(thetaz);
+		rotatez[4] = (float)-sin(thetaz);
+		rotatez[1] = (float)sin(thetaz);
+		rotatez[5] = (float)cos(thetaz);
+	}
+	else if (input == '\\'){
+		if(projvalue){
+			projvalue = false;	
+		}
+		else{
+			projvalue = true;
+		}
+	}
 
         /* Render here */
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
@@ -143,6 +336,26 @@ int main(void) {
 
         // activate shader
         shader.use();
+
+	int ViewLocation = glGetUniformLocation(shader.id(), "View");
+	int ModelLocation = glGetUniformLocation(shader.id(), "Model");
+	int projLocation = glGetUniformLocation(shader.id(), "proj");
+	int rotxLocation = glGetUniformLocation(shader.id(), "rotx");
+	int rotyLocation = glGetUniformLocation(shader.id(), "roty");
+	int rotzLocation = glGetUniformLocation(shader.id(), "rotz");
+	
+	glUniformMatrix4fv(ViewLocation, 1, false, view);
+	glUniformMatrix4fv(ModelLocation, 1, false, model);
+	if(projvalue == true){
+		glUniformMatrix4fv(projLocation, 1, false, project);
+	}
+	else{
+		glUniformMatrix4fv(projLocation, 1, false, ortho);
+	}
+	glUniformMatrix4fv(rotxLocation, 1, false, rotatex);
+	glUniformMatrix4fv(rotyLocation, 1, false, rotatey);
+	glUniformMatrix4fv(rotzLocation, 1, false, rotatez);
+
 
         // render the cube
         glBindVertexArray(VAO);
--- labs/lab4/vert.glsl
+++ labs/lab4/vert.glsl
@@ -3,8 +3,20 @@ layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
 
 out vec3 ourColor;
+uniform mat4 View;
+uniform mat4 Model;
+uniform mat4 proj;
+uniform mat4 rotx;
+uniform mat4 roty;
+uniform mat4 rotz;
+
+mat4 rot = rotx * roty * rotz;
+
+
+vec4 canon = vec4(aPos, 1.0);
+vec4 trans = proj * View * Model * rot * canon;
 
 void main() {
-    gl_Position = vec4(aPos, 1.0);
+    gl_Position = trans;
     ourColor = aColor;
 }
